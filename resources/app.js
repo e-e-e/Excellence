3b841da3c19373a3cc47d93528bfee2abdba516a
e-e-e <e-e-e@users.noreply.github.com>
2015-06-22 12:45:02 +1000

    js +css fixes

--- resources/app.js
+++ resources/app.js
@@ -1,13 +1,18 @@
+
+var reveal_tab = "<div class='reveal_tab'><div>+</div></div>";
+var container = "<div id='sub_menu' class='menu'></div>";
 $(document).ready(function () {
 	console.log('READY');
 	init();
 });
 
 function init () {
-	$('#mw-navigation').children('div').not('#p-navigation').click(menu_show);
+	var r = $(reveal_tab).insertAfter('#p-navigation').click(menu_show);
+	var c = $(container).insertAfter(r);
+	$('#mw-navigation').children('div').not('#p-navigation').appendTo(c).hide();
 }
 
 function menu_show () {
-	console.log('clicked!');
-	$(this).children('ul').toggle();
+	$(this).child().html('-');
+	$('#sub_menu').toggle();
 }
